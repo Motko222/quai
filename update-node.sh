fdcd4a7077d459b8333b4aefa3eb2b85ef8d269e
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-10-08 23:10:59 +0200

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-cd ~/go-quai
-make stop
-git pull
-git fetch --all
+read -p "Sure? " yn1
+case yn1 in
+  y|Y)
+    cd ~/go-quai
+    echo "Stopping..."
+    make stop
+    echo "Updating..."
+    git pull
+    git fetch --all
+    read -p "Clear state? " yn2
+    case yn2 in
+      y|N)
+        echo "Clearing state..."
+        rm -rf nodelogs 
+        rm -rf ~/.quai
+      ;;
+      *)
+        echo "Not clearing state..."
+      ;;
+    esac
+    Echo "Building..."
+    make go-quai
+  ;;
+  *)
+  echo "Cancelled..."
+  ;;
+esac
+
+
+
 make go-quai
